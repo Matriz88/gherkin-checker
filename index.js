1165bcaae6799f7d2db6f1e23b8e7ec7745131b7
Matteo Rizzatti <matteo.rizzatti@yoox.com>
2016-12-14 15:44:15 +0100

    bin provvisorio

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ let Gherkin = require('gherkin');
 let parser = new Gherkin.Parser();
 let tape = require('tape');
 
-let readDirRecurs = require('./utilities').readDirRecurs;
-let readFileAsync = require('./utilities').readFileAsync;
+let readDirRecurs = require('./utils/utilities').readDirRecurs;
+let readFileAsync = require('./utils/utilities').readFileAsync;
 
-let performTest = require('./test-runner');
+let performTest = require('./utils/test-runner');
 let stepsCache = [];
 
 readDirRecurs('features', (err, data) => {
-	if (err) throw err;
+    if (err) throw err;
 
-	tape('Check gherkin components', function (assert) {
-		data.forEach(file => {
-			let fileReadTask = readFileAsync(file);
-			fileReadTask.then(function (data) {
-				let gherkinDocument = parser.parse(data);
-				let compiledGherkin = new Gherkin.Compiler().compile(gherkinDocument);
+    tape('Check gherkin components', assert => {
+        data.forEach(file => {
+            let fileReadTask = readFileAsync(file);
+            fileReadTask.then(data => {
+                let gherkinDocument = parser.parse(data);
+                let compiledGherkin = new Gherkin.Compiler().compile(gherkinDocument);
 
-				compiledGherkin.forEach(function (scenario) {
-					scenario.steps.forEach(function (step) {
-						//skip already checked steps
-						if (stepsCache.indexOf(step.text) > -1) return;
-						stepsCache.push(step.text);
+                compiledGherkin.forEach(scenario => {
+                    scenario.steps.forEach(step => {
+                        //skip already checked steps
+                        if (stepsCache.indexOf(step.text) > -1) return;
+                        stepsCache.push(step.text);
 
-						performTest(step, assert);
-					})
-				})
-			})
-		});
-		assert.end();
-	});
+                        performTest(step, assert);
+                    })
+                })
+            })
+        });
+        assert.end();
+    });
 });
