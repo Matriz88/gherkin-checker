dd30c98465bac2fad2b45cd2f852ddcc4f6df439
Matteo Rizzatti <matriz@hotmail.it>
2016-12-14 20:48:31 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-let Gherkin = require('gherkin');
-let parser = new Gherkin.Parser();
+global.cg_cofig = require('./config/default_config');
+
 let tape = require('tape');
-
 let path = require('path');
-
-let readDirRecurs = require('./lib/utils/utilities').readDirRecurs;
-let readFileAsync = require('./lib/utils/utilities').readFileAsync;
-
+let utils = require('./lib/utils/utilities');
 let performTest = require('./lib/test-runner/test-runner');
 let stepsCache = [];
 
-const config = require('./config/default_config')
-
-readDirRecurs(path.resolve(config.features_path), (err, data) => {
+utils.readDirRecurs(path.resolve(global.cg_cofig.features_path), (err, data) => {
     if (err) throw err;
 
     tape('Check gherkin components', assert => {
         data.forEach(file => {
-            let fileReadTask = readFileAsync(file);
+            let fileReadTask = utils.readFileAsync(file);
             fileReadTask.then(data => {
-                let gherkinDocument = parser.parse(data);
-                let compiledGherkin = new Gherkin.Compiler().compile(gherkinDocument);
-
+                var compiledGherkin = utils.getCompiledGherkin(data);
                 compiledGherkin.forEach(scenario => {
                     scenario.steps.forEach(step => {
                         //skip already checked steps
                         if (stepsCache.indexOf(step.text) > -1) return;
                         stepsCache.push(step.text);
-
                         performTest(step, assert);
                     })
                 })
-            })
+            }).catch((err) => console.log(err.message))
         });
         assert.end();
     });
